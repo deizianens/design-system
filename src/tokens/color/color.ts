b63b9ef1e499c117319463a2d62371493153b234
Deiziane Silva <deiziane_ns@hotmail.com>
2022-06-23 09:55:55 -0300

    adds color and part of typography tokens

---
+++ src/tokens/color/color.ts
@@ -0,0 +1,116 @@
+// PRIMARY COLORS
+export const white = "#FFFFFF";
+export const black = "#000000";
+
+export const blue = {
+  a01: "#bfdfff",
+  a02: "#93c4f5",
+  a03: "#6aaaeb",
+  a04: "#4392e0",
+  a05: "#207bd6",
+  a06: "#0066CC",
+  a07: "#0059b3",
+  a08: "#004d99",
+  a09: "#004080",
+  a10: "#003366",
+  a11: "#00264d",
+  a12: "#001a33",
+};
+
+// SECONDARY COLORS
+export const teal = {
+  a01: "#ccfffd",
+  a02: "#9ff5f2",
+  a03: "#75ebe7",
+  a04: "#4fe0dc",
+  a05: "#2bd6d0",
+  a06: "#0accc6",
+  a07: "#09b3ad",
+  a08: "#089994",
+  a09: "#06807b",
+  a10: "#056663",
+  a11: "#044d4a",
+  a12: "#033331",
+};
+
+//NEUTRAL COLORS
+export const bluegray = {
+  a01: "#EBECED",
+  a02: "#D9DADB",
+  a03: "#C5C7C9",
+  a04: "#A2ADB8",
+  a05: "#959DA6",
+  a06: "#768594",
+  a07: "#5C6F82",
+  a08: "#455B71",
+  a09: "#2F475E",
+  a10: "#17324D",
+};
+
+export const gray = {
+  a01: "#E6ECF2",
+  a02: "#BCC4CC",
+  a03: "#9AA0A6",
+  a04: "#737680",
+  a05: "#535659",
+  a06: "#2f3133",
+  a07: "#18191A",
+};
+
+// COMPLEMENTARY COLORS
+export const indigo = {
+  a07: "#3126ff",
+};
+
+export const red = {
+  a01: "#ffedf0",
+  a02: "#F5D0D6",
+  a03: "#EBA4AF",
+  a04: "#E07B8B",
+  a05: "#D65669",
+  a06: "#cc334a",
+  a07: "#B32D41",
+  a08: "#992637",
+  a09: "#80202e",
+  a10: "#661A25",
+  a11: "#4D131C",
+  a12: "#330D13",
+};
+
+export const emerald = {
+  a01: "#bfffe9",
+  a02: "#93f5d3",
+  a03: "#6aebbd",
+  a04: "#43e0a9",
+  a05: "#20d696",
+  a06: "#00cc85",
+  a07: "#00b374",
+  a08: "#009963",
+  a09: "#008053",
+  a10: "#006642",
+  a11: "#004d32",
+  a12: "#003321",
+};
+
+export const orange = {
+  a01: "#FFE5BF",
+  a02: "#f5ce93",
+  a03: "#EBB76A",
+  a04: "#E0A243",
+  a05: "#D68D20",
+  a06: "#cc7a00",
+  a07: "#B36B00",
+  a08: "#995C00",
+  a09: "#804D00",
+  a10: "#663D00",
+  a11: "#4D2E00",
+  a12: "#331F00",
+};
+
+// SOFT COLORS
+export const lightblue = {
+  a01: "#E8F2FC",
+  a02: "#EDF5FC",
+  a03: "#F2F7FC",
+  a04: "#F5F9FC",
+};
---
+++ src/tokens/typography/font.ts
@@ -0,0 +1,35 @@
+export const fontFamily = {
+  a01: "#bfdfff",
+  a02: "#93c4f5",
+  a03: "#6aaaeb",
+  a04: "#4392e0",
+  a05: "#207bd6",
+  a06: "#0066CC",
+  a07: "#0059b3",
+  a08: "#004d99",
+  a09: "#004080",
+  a10: "#003366",
+  a11: "#00264d",
+  a12: "#001a33",
+};
+
+export const fontSize = {
+  1: "0.875rem",
+  2: "1rem",
+  3: "1.125rem",
+  4: "1.25rem",
+  5: "1.5rem",
+  6: "1.75rem",
+  7: "2rem",
+  8: "2.5rem",
+  9: "3rem",
+  10: "3.5rem",
+};
+
+export const fontWeight = {
+  extraLight: 200,
+  light: 300,
+  regular: 400,
+  semibold: 600,
+  bold: 700,
+};
