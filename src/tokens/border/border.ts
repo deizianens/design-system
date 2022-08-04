aab966b936f15a21bdfe70f7281f4baf3992355b
Deiziane Silva <deiziane_ns@hotmail.com>
2022-08-04 16:56:42 -0300

    adds tokens for border

---
+++ src/tokens/border/border.ts
@@ -0,0 +1,14 @@
+export const radius = {
+  smooth: "4px",
+  rounded: "40px",
+  circle: "100%",
+};
+
+export const stroke = {
+  xs: "1px",
+  sm: "2px",
+  md: "4px",
+  lg: "8px",
+  xl: "16px",
+  xxl: "32px",
+};
